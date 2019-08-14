35b2e2c43f1dfe488dea41b9b6202dcf37acf4d2
BEAN <22887835+bean1310@users.noreply.github.com>
2019-08-15 01:41:21 +0900

    CPUカードのテンプレート追加

---
+++ resources/views/layouts/cpu.blade.php
@@ -0,0 +1,13 @@
+<div class="col">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">CPU</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ $cpuInfo["modelName"] }}</h6>
+            <p class="card-text">
+            Core(s): {{ $cpuInfo["cores"] }}<br>
+            Temp: {{ $cpuInfo["temp"] }}℃<br>
+            {{-- Clock:  --}}
+            </p>
+        </div>
+    </div>
+</div>
