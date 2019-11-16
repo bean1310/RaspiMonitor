8e2117c8273b324c402fc72cab7c41951a47e242
BEAN <22887835+bean1310@users.noreply.github.com>
2019-11-17 00:57:42 +0900

    #16 Software Controlの非表示化

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -37,29 +37,28 @@
         </div>
     </div>
 <br>
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">SoftwareControl</div>
                 <div class="card-body">
-                    {{-- <form action="/home" method="POST">
-                        @csrf --}}
+                    <form action="/home" method="POST">
+                        @csrf 
                         <div class="row justify-content-center">
                             <div class="col text-center">
-                                <button type="submit" class="btn btn-primary" name="updateAndUpgrade">update & upgrade</button>
+                                <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#updateAndUpgradeModal">update & upgrade</button>
                             </div>
                             <div class="col text-center">
                                 <button type="submit" class="btn btn-warning" name="distUpgrade">dist upgrade</button>
                             </div>
                         </div>
-                    {{-- </form> --}}
                 </div>
             </div>
         </div>
     </div>
 </div>
-<br>
+<br> --}}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -80,6 +79,42 @@
     </div>
 </div>
 <br>
+
+<!-- Modal -->
+{{-- <div class="modal fade" id="updateAndUpgradeModal" tabindex="-1" role="dialog" aria-labelledby="updateAndUpgradeModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="updateAndUpgradeModal">update & upgrade</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <h4>
+                <i class="far fa-check-circle fa-lg" style="vertical-align: middle; color:forestgreen"></i>
+                -
+                update
+            </h4>
+            <h4>
+                <div class="spinner-border text-danger" style="width: 1.3em; height: 1.3em;" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+                -
+                upgrade
+            </h4>
+        </div>
+        <div class="modal-footer">
+            <div id="cancel-reboot-process">
+                <cancel-shutdown-process-component></cancel-shutdown-process-component>
+            </div>
+            <div id="quick-poweroff">
+                <quick-poweroff-component></quick-poweroff-component>
+            </div>
+        </div>
+        </div>
+    </div>
+</div> --}}
 
 <!-- Modal -->
 <div class="modal fade" id="poweroffModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
