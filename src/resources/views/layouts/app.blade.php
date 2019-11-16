4dc69b5cbc7c13dec638c277c3f1dc83d7972997
BEAN <22887835+bean1310@users.noreply.github.com>
2019-11-17 01:18:50 +0900

    #20 謎に増えたやつをなんとかする
    わからん。なんやこれ

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/loadingAnimations.css') }}" rel="stylesheet">
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 </head>
 <body>
     <div id="app">
